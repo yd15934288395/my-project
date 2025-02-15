ac8059ec493502f446c9aa1ab5578db8ff4818e7
杨栋 <2048867447@qq.com>
2025-02-15 16:16:35 +0800

    2

---
+++ ConsoleApp1/sdsdsds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class sdsdsds
+    {
+        public void AA()
+        {
+            Console.WriteLine("我是小明");
+        }
+    }
+}
